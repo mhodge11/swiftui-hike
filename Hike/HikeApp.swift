407f56759ab2e79c886b91827684f28793c17bb3
mhodge11 <micahhodgeutk@icloud.com>
2023-07-13 16:05:06 -0400

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Micah Hodge on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
