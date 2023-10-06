a4dba454b5eb9273c1bfa0a65a8679268605724e
mhodge11 <micahhodgeutk@icloud.com>
2023-10-06 19:15:08 -0400

    Initial commit

---
+++ Hike/Utility/GradientButtonStyle.swift
@@ -0,0 +1,25 @@
+//
+//  GradientButtonStyle.swift
+//  Hike
+//
+//  Created by Micah Hodge on 7/13/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional Statement with Nil Coalescing
+                // Condition ? A : B
+                configuration.isPressed ?
+                LinearGradient(colors: [.customGrayMedium, .customGrayLight], startPoint: .top, endPoint: .bottom) :
+                    LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom))
+            .cornerRadius(40)
+    }
+}
